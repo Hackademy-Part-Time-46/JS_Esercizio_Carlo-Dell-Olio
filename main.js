fa79aaf3207b5ac860f66ee935c29f96f7f05ec7
Carlo Dell’Olio <carlodellolio@Air-di-Carlo.lan>
2024-08-30 15:25:13 +0200

    esercizio javascript

---
+++ main.js
@@ -0,0 +1,74 @@
+// inizio esercizio
+
+let num1 = 8;
+
+alert("il valore assegnato alla variabile num1 è" + " " + num1);
+
+console.log(num1);
+
+let num2 = 6;
+
+alert("il valore assegnato alla variabile num2 è" + " " + num2);
+
+console.log(num2);
+
+let stringa = "Mi chiamo stringa";
+
+alert("il valore assegnato alla variabile stringa è" + " " + stringa);
+
+console.log(stringa);
+
+stringa = [1,2,3,4];
+
+alert("il nuovo valore assegnato alla variabile stringa è" + " " + stringa);
+
+console.log(stringa);
+
+// fine esercizio
+
+// inizializzo variabili
+
+let num3;
+
+let num4;
+
+num3 = 10;
+
+alert("il valore assegnato alla variabile num3 è" + " " + num3);
+
+console.log(num3);
+
+num4 = 11;
+
+alert("il valore assegnato alla variabile num4 è" + " " + num4);
+
+console.log(num4);
+
+// fine
+
+// inizio costanti da variabili
+
+let name = "Carlo";
+
+alert("il valore assegnato alla variabile name è" + " " + name);
+
+name = "Matteo";
+
+alert("il nuovo valore assegnato alla variabile name è" + " " + name);
+
+const userName = name;
+
+console.log(userName);
+
+// fine costanti da variabili
+
+// inizio
+
+const PIGRECO = 3.14159265359;
+
+alert("il valore assegnato alla variabile PIGRECO è" + " " + PIGRECO);
+
+console.log(PIGRECO);
+
+// fine
+
---
+++ readme.md
@@ -0,0 +1,36 @@
+Crea un file HTML e main.js e collegali insieme , una volta fatto: 
+
+
+
+ esegui i seguenti passaggi:
+
+
+
+ - dichiara una variabile num1, e assegnale un numero qualsiasi
+
+ - dichiara una variabile num2, e assegnale un numero qualsiasi 
+
+
+
+- visualizza a schermo “Il valore della variabile num1 e' tot“.  
+
+-Fai la stessa cosa per num2 
+
+
+
+
+
+- dichiara una variabile chiamata stringa, e assegnale come valore una stringa qualsiasi 
+
+- stampa a schermo “Il valore della variabile stringa e' tot“ 
+
+- sostituisci il valore della variabile stringa con un qualsiasi altro valore e poi stampalo a schermo. 
+
+
+
+
+
+- dichiara una costante chiamata PIGRECO, e assegnale un valore di 3.14159265359
+
+ - visualizza a schermo “il valore di PIGRECO e' tot” 
+
